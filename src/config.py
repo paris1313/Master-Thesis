23f70c3beee9dd484f51542ffe4c21d633384eb2
paris1313 <155160591+paris1313@users.noreply.github.com>
2026-05-15 11:06:21 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,23 @@
+RANDOM_STATE = 42
 
+TEST_SIZE = 0.2
+
+TARGET_COLUMN = 'M/E FOC (tons/day)'
+
+FEATURE_COLUMNS = [
+    'Slip SOW (%)',
+    'Wind Force (BF)',
+    'M/E RPM',
+    'SOW (knots)',
+    'M/E Brake Power (KW)',
+    'Trim (m)',
+    'Depth (m)',
+    'True Wind Angle (deg off the bow)',
+    'Engine Speed (knots)',
+    'SOG (knots)',
+    'Sea Currents Speed (knots)',
+    'Sea Currents Angle (deg off the bow)',
+    'Cargo Weight (tons)',
+    'Mean Draft (m)',
+    'Course Drift (deg)'
+]
