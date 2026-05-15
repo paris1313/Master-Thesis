6f851775c8f0f19c304fd6dbcbb3681800b73f28
paris1313 <155160591+paris1313@users.noreply.github.com>
2026-05-15 11:10:43 +0300

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt
 
+
+def plot_predictions(y_true, y_pred):
+
+    plt.figure(figsize=(10, 5))
+
+    plt.plot(y_true, label='Actual')
+    plt.plot(y_pred, label='Predicted')
+
+    plt.legend()
+
+    plt.show()
