24c3eae30426a8f35adf592598b9fc199ded143a
paris1313 <155160591+paris1313@users.noreply.github.com>
2026-05-15 11:08:42 +0300

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,26 @@
+import numpy as np
+from scipy import stats
 
+
+def remove_outliers(df, columns, threshold=2.7):
+
+    for col in columns:
+
+        z_scores = np.abs(stats.zscore(df[col]))
+
+        df = df[z_scores < threshold]
+
+    return df
+
+
+def filter_operational_conditions(df):
+
+    df = df.loc[
+        df['Voyage Status'].str.contains('Sailing') &
+        (df['M/E FLOW METER GROUP ALARM'] != 1) &
+        (df['SOW (knots)'] > 5) &
+        (df['M/E RPM'] > 0) &
+        (df['Depth (m)'] > 70)
+    ]
+
+    return df
