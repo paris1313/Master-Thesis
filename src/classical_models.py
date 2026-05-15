40d98f5782905ca7f2ecbf0b06d0262fd1fcfd48
paris1313 <155160591+paris1313@users.noreply.github.com>
2026-05-15 11:09:03 +0300

    Update classical_models.py

--- src/classical_models.py
+++ src/classical_models.py
@@ -1,1 +1,18 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import ElasticNet
+from sklearn.ensemble import HistGradientBoostingRegressor
 
+
+def get_models():
+
+    return {
+        'LR': LinearRegression(),
+        'RF': RandomForestRegressor(n_estimators=100),
+        'DT': DecisionTreeRegressor(max_depth=5),
+        'GBR': HistGradientBoostingRegressor(),
+        'SVR': SVR(C=1.0, epsilon=0.2),
+        'EN': ElasticNet()
+    }
