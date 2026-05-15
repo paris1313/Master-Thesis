e7990aa4e79416119a9df39089874a289999fc68
paris1313 <155160591+paris1313@users.noreply.github.com>
2026-05-15 11:09:57 +0300

    Update deep_learning.py

--- src/deep_learning.py
+++ src/deep_learning.py
@@ -1,1 +1,60 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import SimpleRNN
+from tensorflow.keras.layers import LSTM
 
+
+def build_ann(input_dim):
+
+    model = Sequential()
+
+    model.add(Dense(64, activation='relu', input_shape=(input_dim,)))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(1))
+
+    model.compile(
+        optimizer='adam',
+        loss='mean_absolute_error'
+    )
+
+    return model
+
+
+def build_rnn(input_shape):
+
+    model = Sequential()
+
+    model.add(SimpleRNN(
+        64,
+        activation='relu',
+        input_shape=input_shape
+    ))
+
+    model.add(Dense(1))
+
+    model.compile(
+        optimizer='adam',
+        loss='mean_absolute_error'
+    )
+
+    return model
+
+
+def build_lstm(input_shape):
+
+    model = Sequential()
+
+    model.add(LSTM(
+        64,
+        activation='relu',
+        input_shape=input_shape
+    ))
+
+    model.add(Dense(1))
+
+    model.compile(
+        optimizer='adam',
+        loss='mean_absolute_error'
+    )
+
+    return model
