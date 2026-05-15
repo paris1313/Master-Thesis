db122bf7ca9e7bca992b4a87ad5772c0ad732d4d
paris1313 <155160591+paris1313@users.noreply.github.com>
2026-05-15 11:07:16 +0300

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,20 @@
+def add_mean_draft(df):
 
+    df['Mean Draft (m)'] = (
+        df['DRAFT MP'] +
+        df['DRAFT MS'] +
+        df['DRAFT FORWARD'] +
+        df['DRAFT AFT']
+    ) / 4
+
+    return df
+
+
+def add_course_drift(df):
+
+    df['Course Drift (deg)'] = (
+        df['Gyro Heading (deg)'] -
+        df['GPS Course (deg)']
+    )
+
+    return df
