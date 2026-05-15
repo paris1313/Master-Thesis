a050093d94b20a1d24de6212e31c47e0f99eb0cd
paris1313 <155160591+paris1313@users.noreply.github.com>
2026-05-15 11:26:45 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,15 @@
+# master_thesis/src/__init__.py
 
+"""
+Master Thesis package.
+
+Machine learning utilities for:
+- Ship fuel oil consumption prediction
+- Data preprocessing
+- Model evaluation
+- Visualization
+"""
+
+__version__ = "1.0.0"
+
+__author__ = "Paris1313"
