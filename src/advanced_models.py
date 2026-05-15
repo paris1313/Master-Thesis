4ca269fa94bd754b5f7401000ee15bdaa9bde92d
paris1313 <155160591+paris1313@users.noreply.github.com>
2026-05-15 11:09:26 +0300

    Update advanced_models.py

--- src/advanced_models.py
+++ src/advanced_models.py
@@ -1,1 +1,18 @@
+from sklearn.linear_model import BayesianRidge
+from sklearn.linear_model import SGDRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
 
+
+kernel = DotProduct() + WhiteKernel()
+
+
+def get_advanced_models():
+
+    return {
+        'Bayesian': BayesianRidge(),
+        'SGD': SGDRegressor(),
+        'KNN': KNeighborsRegressor(n_neighbors=7),
+        'GPR': GaussianProcessRegressor(kernel=kernel)
+    }
